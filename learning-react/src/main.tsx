f39d6ed7fbb168a6f97afc44d4975fce25db024c
ruiwaa <yeejii08@gmail.com>
2026-03-06 13:06:14 +0900

    ready. JSX 학습

--- learning-react/src/main.tsx
+++ learning-react/src/main.tsx
@@ -4,15 +4,24 @@ import "./index.css";
 import App from "./App.tsx";
 
 // 방어적 프로그래밍
-const rootElemnt = document.getElementById("root");
+const rootElement = document.getElementById("root");
 
 // rootElement가 null일 가능성을 방지해주기
-if (!rootElemnt) {
+if (!rootElement) {
   throw new Error("문서에 #root인 요소가 없습니다. 확인해보세요");
 }
 
-createRoot(rootElemnt).render(
+// React(Virtual) DOM (가짜 문서 객체 모델의 뿌리)
+const ReactDOMRoot = createRoot(rootElement);
+// console.log(ReactDOMRoot) // ReactDOMRoot { render, unmount }
+ReactDOMRoot.render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
+// createRoot(rootElement).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
