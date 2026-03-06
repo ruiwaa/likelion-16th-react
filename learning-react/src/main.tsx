26dce2355054162e2b12acdd0f3664226cb0eaf0
ruiwaa <yeejii08@gmail.com>
2026-03-06 11:28:49 +0900

    learn. 방어적 프로그래밍 - rootElement가 null일 가능성을 걷어내자! - TypeScript 타입 가드(유형 보호)

--- learning-react/src/main.tsx
+++ learning-react/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+// 방어적 프로그래밍
+const rootElemnt = document.getElementById("root");
+
+// rootElement가 null일 가능성을 방지해주기
+if (!rootElemnt) {
+  throw new Error("문서에 #root인 요소가 없습니다. 확인해보세요");
+}
+
+createRoot(rootElemnt).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
