78482f85469995b6974f960cba005c69f1685738
ruiwaa <yeejii08@gmail.com>
2026-03-09 14:58:58 +0900

    learn. 조건부 렌더링 -ConditionalRendering - ProfileCard

--- learning-react/02-State-이벤트-핸들링/src/app/index.tsx
+++ learning-react/02-State-이벤트-핸들링/src/app/index.tsx
@@ -1,4 +1,5 @@
-import {Header, Footer, Section, Button} from '@/components'
+import {Header, Footer, Wrapper} from '@/components'
+import { ConditionalRendering } from '@/learns';
 import S from "./style.module.css";
 
 export default function App() {
@@ -7,25 +8,10 @@ export default function App() {
       <Header>
         <h1>내맘대로 헤더!</h1>
       </Header>
-
-      <Header>
-        <h2>내 맘대로 컴포넌트 조립하기!</h2>
-        <p>컴포넌트를 조립해서 쓰니 유지보수가 편해요!</p>
-      </Header>
-      <Section title="리액트는 리액션" isShowTitle>
-        <p>react는 사용자의 행동에 따라 리액션을 제공한다.</p>
-        <div
-          lang="en"
-          style={{ display: "flex", gap: 6, marginBlockStart: 12 }}
-        >
-          <Button onNoti={() => alert("리액션!")}>Reaction</Button>
-          <Button onNoti={() => alert("리액티브!")} isDisabled>
-            Reactive
-          </Button>
-          <Button onNoti={() => alert("리액티비티!")}>Reactivity</Button>
-        </div>
-      </Section>
-      <div data-placeholder />
+      <Wrapper>
+      <ConditionalRendering />
+      </Wrapper>
+      {/* <div data-placeholder /> */}
       <Footer slogan={"완주 이후엔 스스로 학습이 가능!!!!!"} />
     </div>
   );
