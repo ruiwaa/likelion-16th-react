3008f661bd3b016d815294b76f2e3b7139460f4b
ruiwaa <yeejii08@gmail.com>
2026-03-09 17:19:02 +0900

    learn. 이벤트 핸들링 (Event Handling) 정리

--- learning-react/02-State-이벤트-핸들링/src/app/index.tsx
+++ learning-react/02-State-이벤트-핸들링/src/app/index.tsx
@@ -1,5 +1,5 @@
-import {Header, Footer} from '@/components'
-import { ComponentState } from '@/learns'
+import { Footer, Header, Wrapper } from '@/components';
+import { EventHandling } from '@/learns';
 import S from "./style.module.css";
 
 export default function App() {
@@ -8,11 +8,11 @@ export default function App() {
       <Header>
         <h1>컴포넌트 디자인</h1>
       </Header>
-      {/* <Wrapper>
-      <ConditionalRendering />
-      </Wrapper> */}
-      {/* <div data-placeholder /> */}
-      <ComponentState/>
+       <Wrapper>
+        <EventHandling/>
+      {/* <ConditionalRendering /> */}
+      {/* <ComponentState/> */}
+      </Wrapper> 
 
       <Footer slogan={"완주 이후엔 스스로 학습이 가능!!!!!"} />
     </div>
