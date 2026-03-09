f331c996e7a154b18a17ca823d5e117b794a9d7b
ruiwaa <yeejii08@gmail.com>
2026-03-09 10:33:07 +0900

    learn. 컴포넌트 속성(Props) - Props는 객체 타입 - TypeScript 인라인 타입 정의

--- learning-react/02-State-이벤트-핸들링/src/app/index.tsx
+++ learning-react/02-State-이벤트-핸들링/src/app/index.tsx
@@ -1,5 +1,19 @@
+import Footer from '@/components/Footer'
 import S from './style.module.css'
+//import { createElement } from 'react'
 
 export default function App() {
-  return <div className={S.container}></div>
+  return (<div className={S.container}>
+    <h1>React App</h1>
+    <Footer slogan ={'완주 이후엔 스스로 학습이 가능!'}/>
+    
+   {/* {createElement(
+     // Component Name 
+      Footer,
+     // Component Props 
+    { slogan: '완주 이후엔 스스로 학습이 가능!'},
+      )} */}
+
+  </div>
+  )
 }
--- learning-react/02-State-이벤트-핸들링/src/main.tsx
+++ learning-react/02-State-이벤트-핸들링/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@/app'
 import '@/styles/main.css'
@@ -10,7 +10,7 @@ if (!rootElement) {
 }
 
 createRoot(rootElement).render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
+  </>,
 )
