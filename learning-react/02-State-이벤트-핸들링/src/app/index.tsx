ad699a2b61c509aefa370caec701df8ce4725a3a
ruiwaa <yeejii08@gmail.com>
2026-03-10 09:49:28 +0900

    learn. 이벤트 핸들러 클로저(Closure) 활용

--- learning-react/02-State-이벤트-핸들링/src/app/index.tsx
+++ learning-react/02-State-이벤트-핸들링/src/app/index.tsx
@@ -1,5 +1,5 @@
 import { Footer, Header, Wrapper } from '@/components';
-import { EventHandling } from '@/learns';
+import { EventHandling, ComponentState } from '@/learns';
 import S from "./style.module.css";
 
 export default function App() {
@@ -9,9 +9,9 @@ export default function App() {
         <h1>컴포넌트 디자인</h1>
       </Header>
        <Wrapper>
+      <ComponentState/>
         <EventHandling/>
       {/* <ConditionalRendering /> */}
-      {/* <ComponentState/> */}
       </Wrapper> 
 
       <Footer slogan={"완주 이후엔 스스로 학습이 가능!!!!!"} />
