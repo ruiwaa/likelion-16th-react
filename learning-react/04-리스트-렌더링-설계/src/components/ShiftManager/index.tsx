adb044d34cb7c0fbbf73dd121d829129d9fd1434
ruiwaa <yeejii08@gmail.com>
2026-03-12 14:11:24 +0900

    learn. 리스트 렌더링과 유니크한 Key의 중요성 - key = 리스트 아이템 간 고유한 식별자 - 항목의 순서가 바뀌어야 할 경우, 필히 고유 값 사용 (예: id 등) - map() 메서드의 index는 최후의 수단!!

--- learning-react/04-리스트-렌더링-설계/src/components/ShiftManager/index.tsx
+++ learning-react/04-리스트-렌더링-설계/src/components/ShiftManager/index.tsx
@@ -1,7 +1,24 @@
 import S from './style.module.css'
+import type { Manager } from './type/manager'
+import managersData from './data/managers.json'
+import { useState } from 'react'
+
+const INITIAL_MANAGERS: Manager[] = managersData
 
 export default function ShiftManager() {
+
+  // 매니저들 상태 선언
+  const [managers, setManagers] =useState(INITIAL_MANAGERS)
+   
+
+  const handleShuffleCard = () => {
+    const nextManagers = managers.toSorted(() => Math.random() - 0.5)
+    setManagers(nextManagers)
+    
+  }
+
   return (
+
     <section className={S.container}>
       <header className={S.header}>
         <h2 id="manager-title">오늘의 근무 순서 조정</h2>
@@ -13,10 +30,32 @@ export default function ShiftManager() {
           type="button"
           className={S.reorderBtn}
           aria-controls="staff-list"
+          onClick={handleShuffleCard}
         >
           근무 순서 무작위 재배치
         </button>
       </header>
+      <ul className={S.list} id='staff-list'>
+      {managers.map((manager) => {
+        return (
+          <li key={manager.id} className={S.card}>
+            <div className={S.staffInfo}>
+              <span className={S.badge}>{manager.task}</span>
+              <strong className={S.name}>{manager.name}</strong>
+            </div>
+            <div className={S.notice}>
+              <span className={S.label}>공지: {manager.message}</span>
+            </div>
+
+            <div className={S.memoField}>
+              <label htmlFor={manager.id}>{manager.name}님 전달사항</label>
+              <input type="text" id={manager.id} className={S.input}
+              placeholder={`${manager.name}님에게 전달할 메모`}/>
+            </div>
+          </li>
+        )
+      })}
+      </ul>
     </section>
   )
 }
