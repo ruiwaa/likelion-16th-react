552d906dd5aee2d5e6f88c0ef1d6921d161f6e44
ruiwaa <yeejii08@gmail.com>
2026-03-12 15:56:50 +0900

    learn. 컴포넌트 추출 1 - 유틸리티 함수 - 데이터 - 타입

--- learning-react/04-리스트-렌더링-설계/src/app/index.tsx
+++ learning-react/04-리스트-렌더링-설계/src/app/index.tsx
@@ -1,10 +1,10 @@
-import { ScheduleTable } from '@/components'
-import S from './style.module.css'
+import { ProductList } from "@/components";
+import S from "./style.module.css";
 
 export default function App() {
   return (
     <div className={S.container}>
-      <ScheduleTable/>
+      <ProductList />
     </div>
-  )
+  );
 }
