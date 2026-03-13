8c05917cbb65be790de6639b0563ad97e03eb0d8
ruiwaa <yeejii08@gmail.com>
2026-03-13 17:29:04 +0900

    learn. 이펙트 클린업(정리) 함수

--- learning-react/05-이펙트-비동기-통신/src/components/EffectCleanup/EffectCleanup.tsx
+++ learning-react/05-이펙트-비동기-통신/src/components/EffectCleanup/EffectCleanup.tsx
@@ -1,7 +1,11 @@
+import { useEffect, useState } from 'react'
 import S from './EffectCleanup.module.css'
 
 export default function EffectCleanup() {
-  
+  //타이머 표시/감춤 상태 선언
+  const [isShow, setIsShow] =useState(false)
+
+
   return (
     <article className={S.container}>
       <header>
@@ -9,19 +13,42 @@ export default function EffectCleanup() {
         <p>컴포넌트의 소멸과 자원 정리 과정을 관찰합니다.</p>
       </header>
 
-      <button type="button" className={S.toggleButton}>
-        {'타이머 추가'}
+      <button type="button" className={S.toggleButton} onClick={() => setIsShow(!isShow)} aria-pressed = {isShow}>
+       타이머 {isShow? '제거' : '추가'}
       </button>
 
       <div role="region" aria-live="polite">
-        <Timer />
+        {isShow? <Timer /> : null}
       </div>
     </article>
   )
 }
 
 function Timer() {
+
+  //리액트의 상태 선언 (상태 = 반응성 데이터 = 변경되면 리액트가 반응한다.)
+  const [seconds, setSeconds] = useState(0)
   
+  // 특정 시간마다 콜백 함수를 실행하는 방법
+  // 리액트 관점에서 보면 타이머는 리액트의 렌더링과 관련이 없음
+  // 타이머 조작은 side Effect 이다.  
+  // 그러므로 useEffect 사용
+  useEffect(() => {
+    // 오직 처음 렌더링할 때 한 번만 타이머를 가동하면 된다.
+    // 1초마다 실행되도록 제공하는 web API : setInterval
+
+    const intervalId = setInterval(() => {setSeconds((s) => s +1)},1000)
+
+    return () => {
+      // 여기서 정리
+      // 예) 타이머 정리
+      clearInterval(intervalId)
+      console.log('타이머 클린업');
+      
+    }
+  },[])
+
+
   return (
     <div className={S.card}>
       <p className={S.info}>실시간 타이머</p>
@@ -30,7 +57,7 @@ function Timer() {
         aria-live="polite" 
         aria-atomic="true"
       >
-        {0}s
+        {seconds}s
       </output>
       <span className={S.info}>
         이 카드가 사라지면 콘솔 로그도 멈춰야 합니다.
--- learning-react/05-이펙트-비동기-통신/src/components/EffectDependencies/EffectDependencies.tsx
+++ learning-react/05-이펙트-비동기-통신/src/components/EffectDependencies/EffectDependencies.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import S from './EffectDependencies.module.css'
 
 export default function EffectDependencies() {
@@ -8,33 +8,33 @@ export default function EffectDependencies() {
   // - 텍스트 상태
   const [text, setText] = useState('')
   // - 마운트 상태
-  const [mounted] = useState(false)
+  const [mounted, setMounted] = useState(false) // '리액트 앱이 아직 마운트 전이다'
 
-
-
-  // 이펙트 설정 (종속성 또는 의존성)
-  // 상황 1 의존성 배열이 없을 때
-  // 컴포넌트가 렌더링 할 때 마다 이펙트 함수의 로직이 실행됨
-  // useEffect(() => {
-  //   document.title = `⌨️ ${text.length === 0 ? '입력 대기 중 ...' : text}`
-  //   console.log('외부의 시스템인 브라우저와 리액트의 상태를 동기화했습니다. 👏');
-    
-  // })
-
-  // 상황 2 의존성 배열이 비어있는 경우
-  // 최초 렌더링 할 때 단 한번만 이펙트 함수의 로직이 실행됨
+  // 이펙트 설정 (종속성 또는 의존성(dependencies: Array)
+  // 상황 1. 의존성 배열이 없을 경우
+  // 컴포넌트가 렌더링 할 때 마다 이펙트 함수의 로직이 실행
   useEffect(() => {
-    const currentYear = new Date().getFullYear()
-    setCount(currentYear)
-    console.log(`올해는 ${currentYear} 입니다.`);
-  }, []) // 종속성을 비워줌
+    if (text.length > 5) return
+    document.title = `⌨️ ${text.length === 0 ? '입력 대기 중...' : text}`
+    console.log('외부 시스템인 브라우저와 리액트의 상태를 동기화했습니다! ✨')
+  })
 
-  // 상황 3 의존성 배열에 특정 값이 포함된 경우
-  // 종속성 배열에 반응성 데이터를 추가하면 오직 그 데이터가 변경될 때만 함수 로작이 실행됨.
+  // 상황 2. 의존성 배열이 비어있는 경우
+  // 최초 렌더링 할 때 단 한번만 이펙트 함수의 로직이 실행
   useEffect(() => {
-    console.log(`현재 카운트 값 = ${count}`);
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setMounted(true) // '리액트 앱이 마운트 되었다!'
+
+    // const currentYear = new Date().getFullYear()
+    // setCount(currentYear) // 0 -> 2026
+    // console.log(`올해는 ${currentYear}년입니다. 📆`)
+  }, [])
+
+  // 상황 3. 의존성 배열에 특정 값이 포함된 경우
+  // 종속성 배열에 반응성 데이터를 추가하면 오직 그 데이터가 변경될 때만 이펙트 함수의 로직이 실행됨
+  useEffect(() => {
+    console.log(`현재 카운트 값 = ${count}`)
     document.title = `현재 카운트 = ${count}`
-    
   }, [count])
 
   return (
@@ -68,7 +68,7 @@ export default function EffectDependencies() {
             placeholder="여기에 타이핑해도 Case 3은 실행되지 않습니다."
             aria-describedby="input-help"
             value={text}
-            onChange={(e)=> {setText(e.target.value)}}
+            onChange={(e) => setText(e.target.value)}
           />
           <p id="input-help" className="sr-only">
             이 입력창의 변화는 특정 의존성 배열이 설정된 Effect를 트리거하지
@@ -81,7 +81,7 @@ export default function EffectDependencies() {
         <p>콘솔창(F12)을 확인하며 Effect의 동작을 관찰하세요.</p>
         <p>
           현재 입력 내용:
-          <ins className={S.textHighlight}>{text ||'입력 대기 중...'}</ins>
+          <ins className={S.textHighlight}>{text || '입력 대기 중...'}</ins>
         </p>
       </footer>
     </article>
