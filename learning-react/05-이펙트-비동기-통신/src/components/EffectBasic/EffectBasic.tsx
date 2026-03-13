5007140829a96007a0014a727ff792ec9911f38e
ruiwaa <yeejii08@gmail.com>
2026-03-13 15:30:05 +0900

    learn. 이펙트(Effect) 기초 - 순수 함수 (리액트는 컴포넌트를 순수하게 유지하길 요구) - StrictMode의 역할 (개발 모드에서만 두 번 컴포넌트 실행해 순수한지 여부를 확인하여 문제가 발생할 수 있는 가능성을 개발자에게 안내) - 이펙트(설정함수): 렌더링 단계에서 벗어난 외부 시스템과 동기화 로직

--- learning-react/05-이펙트-비동기-통신/src/components/EffectBasic/EffectBasic.tsx
+++ learning-react/05-이펙트-비동기-통신/src/components/EffectBasic/EffectBasic.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from 'react'
 import styles from './EffectBasic.module.css'
 
+
+
+async function wait(delay = 1000) {
+  return new Promise((resolve) => setTimeout(resolve, delay))
+  
+}
 export default function EffectBasic() {
+  
+  const [count, setCount] = useState(0)
+  
+
+  const handleIncreament = async() => {
+    await wait(1500)
+    setCount((nextCount)=> nextCount + 1)
+  }
+
+  useEffect(() => {
+
+    document.title = `현재 카운터:${count}`
+  })
+
+
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>Effect 기초 학습</h2>
 
-      <button type="button" className={styles.counterButton}>
-        카운트 증가: 0
+      <button type="button" className={styles.counterButton} onClick={handleIncreament} >
+        카운트 증가: {count}
       </button>
 
       <p className={styles.statusText}>
