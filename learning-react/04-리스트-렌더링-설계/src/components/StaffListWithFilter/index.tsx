a9a841bd5cf8c72b71ac824d473732860d1aaf23
ruiwaa <yeejii08@gmail.com>
2026-03-13 12:00:26 +0900

    learn. StaffListWithFilter 필터링 구현 파트 2

--- learning-react/04-리스트-렌더링-설계/src/components/StaffListWithFilter/index.tsx
+++ learning-react/04-리스트-렌더링-설계/src/components/StaffListWithFilter/index.tsx
@@ -8,12 +8,22 @@ import StaffListHeader from "./parts/StaffListHeader";
 
 export default function StaffListRender() {
   const [staffs] = useState<Staff[]>(StaffData);
+  const [search, setSearch] = useState("");
+
+  //파생된 상태(데이터)
+  //검색어에 의해 걸러진 스태프 목록
+  const searchedStaffs = staffs.filter((staff) => {
+    const hasName = staff.name.toLowerCase().includes(search.toLocaleLowerCase().trim());
+    const hasRole = staff.role.toLowerCase().includes(search.toLocaleLowerCase().trim());
+    const hasPhone = staff.phone.toLowerCase().includes(search.toLocaleLowerCase().trim());
+    return hasPhone || hasName || hasRole
+  });
 
   return (
     <section className={S.container}>
-      <StaffListHeader staffs={staffs} />
-      <StaffListSearch />
-      <StaffList staffs={staffs} />
+      <StaffListHeader staffs={staffs}  searchedStaffs ={searchedStaffs}/>
+      <StaffListSearch search={search} setSearch={setSearch} />
+      <StaffList staffs={searchedStaffs} />
     </section>
   );
 }
