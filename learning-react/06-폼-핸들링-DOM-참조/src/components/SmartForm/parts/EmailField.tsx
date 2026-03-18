5b3855f24f594fa17db001714bd2e2b03f0c0b37
ruiwaa <yeejii08@gmail.com>
2026-03-18 09:05:29 +0900

    추가: 수정된 소스 파일

--- learning-react/06-폼-핸들링-DOM-참조/src/components/SmartForm/parts/EmailField.tsx
+++ learning-react/06-폼-핸들링-DOM-참조/src/components/SmartForm/parts/EmailField.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useId } from 'react'
+import { useId, useState } from 'react'
 import S from '../SmartForm.module.css'
 
+// 이메일 검사를 위한 정규식
 const EMAIL_PATTERN = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
 interface Props {
@@ -11,17 +11,18 @@ interface Props {
 
 export default function EmailField({ value, onChange }: Props) {
   const filedId = useId()
-  // TODO 1: 에러 메시지와 '필드 방문 여부'를 관리할 상태를 만드세요.
+  const messageId = useId()
   
+  const [isTouched, setIsTouched] = useState(false)
 
-  const handleBlur = () => {
-    // TODO 2: 사용자가 필드를 벗어나는 순간(Blur), 방문 여부를 true로 바꾸고 검증을 실행하세요.
-    // 힌트: EMAIL_PATTERN 정규식을 활용하세요.
-
+  const getErrorMessage = () => {
+    if (!isTouched) return ''
+    if (!value) return '이메일을 입력하세요.'
+    return EMAIL_PATTERN.test(value) ? '' : '유효한 이메일이 아닙니다.'
   }
 
-  // TODO 3: '방문했고(isTouched)' + '에러가 있을 때'만 에러를 보여주도록 변수를 설정하세요.
-  const showError = false // 이 부분을 완성하세요.
+  const error = getErrorMessage()
+  const showError = error !== ''
 
   return (
     <div className={S.field}>
@@ -32,17 +33,21 @@ export default function EmailField({ value, onChange }: Props) {
         id={filedId}
         type="email"
         placeholder="user@email.com"
-        value={value}
+        className={showError ? S.inputError : S.input}
+        aria-invalid={showError ? 'true' : 'false'}
+        aria-describedby={messageId}
         onChange={(e) => onChange(e.target.value)}
-        // TODO 4: onBlur 이벤트를 연결하고, 에러 상태에 따라 클래스와 aria 속성을 제어하세요.
-        
-        className={S.input}
+        onBlur={() => setIsTouched(true)}
+        value={value}
       />
 
-      {/* TODO 5: showError가 true일 때만 에러 메시지(p 태그)를 렌더링하세요. */}
-      {/* <p role="alert" className={S.errorMessage}>
-        {'에러 메시지'}
-      </p> */}
+      {showError ? (
+        <p id={messageId} role="alert" className={S.errorMessage}>
+          {error}
+        </p>
+      ) : (
+          <p id={messageId} className={S.infoMessage}>올바른 이메일 주소 입력</p>
+        )}
     </div>
   )
 }
