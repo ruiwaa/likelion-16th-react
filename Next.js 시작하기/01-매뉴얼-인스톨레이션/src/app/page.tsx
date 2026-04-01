6654c38bb80dc8206c24238722390898670a828e
ruiwaa <yeejii08@gmail.com>
2026-04-01 09:25:29 +0900

    추가 1번 학습폴더

--- Next.js 시작하기/01-매뉴얼-인스톨레이션/src/app/page.tsx
+++ Next.js 시작하기/01-매뉴얼-인스톨레이션/src/app/page.tsx
@@ -1,8 +1,10 @@
+import VercelLogo from '@/components/ui/vercel-logo'
+
 export default function Page() {
   return (
-    <header className="flex min-h-screen flex-col items-center justify-center bg-slate-50">
-      <h1 className="text-forground text-4xl font-extralight">안녕 Next.js </h1>
-      <img src="/vercel.svg" alt="versel 로고" className="mt-2 size-8" />
+    <header className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-black">Hello Next.js</h1>
+      <VercelLogo className="mt-2 size-8 text-rose-500 dark:text-yellow-400" />
     </header>
   )
 }
