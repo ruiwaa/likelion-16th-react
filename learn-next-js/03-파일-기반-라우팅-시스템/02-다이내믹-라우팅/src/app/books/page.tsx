d5a16e973cc47aeefb7b5c6a1d3c1ab95228be28
ruiwaa <yeejii08@gmail.com>
2026-04-03 18:29:07 +0900

    learn. 다이내믹 세그먼트 - [dynamic segment] - Service 함수 - 리스트 렌더링(연습)

--- learn-next-js/03-파일-기반-라우팅-시스템/02-다이내믹-라우팅/src/app/books/page.tsx
+++ learn-next-js/03-파일-기반-라우팅-시스템/02-다이내믹-라우팅/src/app/books/page.tsx
@@ -2,17 +2,32 @@ import { LucideTrophy } from 'lucide-react'
 
 import LinkCard from '@/components/ui/link-card'
 import PageSectionTitle from '@/components/ui/page-section-title'
-
+import { getAllBooks } from '@/services/books'
+import Link from 'next/link'
 
 export default async function BooksPage() {
-
+  const allbooks = await getAllBooks()
   return (
     <div className="mx-auto space-y-8">
       <PageSectionTitle
         title="북 아카이브"
         description="현재 큐레이션 된 도서 목록입니다. 당신의 인생 책을 찾아보세요."
       />
-
+      {/* ISBN 정보를 가진 데이터를 순환해 리스트 렌더링 */}
+      <ul className="flex flex-col gap-y-6">
+        {allbooks?.map((book) => (
+          <li key={book.isbn} className="flex flex-col items-center gap-3">
+            <Link href={`books/${book.isbn}`}>
+              <img
+                src={book.image}
+                alt={book.title}
+                className="w-80 grow transition-transform duration-500 hover:-translate-y-5"
+              />
+            </Link>
+            <p className="text-xl">{book.title}</p>
+          </li>
+        ))}
+      </ul>
       <LinkCard
         href="/books/best"
         title="베스트셀러"
